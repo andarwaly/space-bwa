e26dae0581582ccd650ccc63e9660c0c2b8f8938
Muhammad Dzaky Waly Andarwa <andarwaly@gmail.com>
2022-04-13 15:12:17 +0700

    Sign in Page, email pass widget

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 import 'package:space/theme.dart';
 
 class SignInPage extends StatelessWidget {
@@ -7,7 +8,110 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('test'),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          title(),
+          emailInput(),
+          passInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget title() {
+    return Container(
+      width: 160,
+      margin: EdgeInsets.only(
+        top: 84,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Login to your\naccount',
+              style: blackAccentText.copyWith(
+                fontSize: 24,
+                fontWeight: bold,
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                  width: 87,
+                  height: 4,
+                  margin: EdgeInsets.only(right: 4),
+                  decoration: BoxDecoration(
+                    color: kBlackColor,
+                    borderRadius: BorderRadius.circular(4),
+                  )),
+              Container(
+                width: 8,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: kBlackColor,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget emailInput() {
+    return Container(
+      margin: const EdgeInsets.only(top: 48),
+      padding: const EdgeInsets.all(16),
+      width: 366,
+      decoration: BoxDecoration(
+        color: kWhiteGray,
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: TextFormField(
+        decoration: InputDecoration.collapsed(
+            hintText: 'Email',
+            hintStyle: grayText.copyWith(
+              fontSize: 16,
+              fontWeight: semiBold,
+            )),
+      ),
+    );
+  }
+
+  Widget passInput() {
+    return Container(
+      margin: const EdgeInsets.only(top: 24),
+      padding: const EdgeInsets.all(16),
+      width: 366,
+      decoration: BoxDecoration(
+        color: kWhiteGray,
+        borderRadius: BorderRadius.circular(14),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextFormField(
+              obscureText: true,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Password',
+                hintStyle: grayText.copyWith(
+                  fontSize: 16,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ),
+          ),
+          const Icon(
+            LucideIcons.eye,
+            color: kGrayColor,
+          )
+        ],
+      ),
     );
   }
 }
